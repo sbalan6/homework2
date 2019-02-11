8a3bd57428647c184f3771afce09a9d139287010
sbalan6 <47480891+sbalan6@users.noreply.github.com>
2019-02-10 19:12:55 -0600

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -1,1 +1,23 @@
-int main(){return 0;}
+ #include "csapp.h"
+ #define MAXARGS 128
+
+ /* Function prototypes */
+ void eval(char *cmdline);
+ int parseline(char *buf, char **argv);
+ int builtin_command(char **argv);
+
+ int main()
+ {
+ char cmdline[MAXLINE]; /* Command line */
+
+ while (1) {
+ /* Read */
+ printf("> ");
+ Fgets(cmdline, MAXLINE, stdin);
+ if (feof(stdin))
+ exit(0);
+
+ /* Evaluate */
+ eval(cmdline);
+ }
+ }
